f6b8cb74bdf9e5f85e5c01bb3c654551c7d503e1
FedericoManzano <federicomanzano111@gmail.com>
2020-07-07 08:11:52 -0300

    Estilos del Tips

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,8 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }
+      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' },
+      { test: /\.css$/, loader: "style-loader!css-loader" }
     ]
   }
 };
