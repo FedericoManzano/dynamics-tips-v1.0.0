591be6f095aa9c53489557dc32826dc6603c8c94
FedericoManzano <federicomanzano111@gmail.com>
2020-07-08 07:14:28 -0300

    Dropdown

---
+++ src/modulos/DropDown.js
@@ -0,0 +1,31 @@
+
+import $ from "jquery";
+import  "../../css/estilos-dynamics.css";
+import Posicionamiento from "./Posicionamiento"
+
+
+(function () {
+
+
+    const eventoClick = (e) => {
+        
+    }
+
+    const eventoHover= (e) => {
+        
+    }
+
+
+    const inicializar = () => {
+        
+       
+    }
+
+    const DropDown = {
+        iniciar: () => inicializar()
+    }
+
+    window.DropDown = DropDown
+}) ()
+
+export default DropDown
